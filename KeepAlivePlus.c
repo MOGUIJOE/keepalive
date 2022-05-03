2f001bb20c207332a7556a9e4e47658c8aea3b63
omgxixi <103768165+omgxixi@users.noreply.github.com>
2022-05-03 13:38:15 +0800

    Add files via upload

---
+++ KeepAlivePlus.c
@@ -0,0 +1,5 @@
+#include <stdlib.h>
+#include <stdio.h>
+void main(){
+    printf("%s","保活加QQ:2226864272");
+}
---
+++ KeepAlivePlus.java
@@ -0,0 +1,5 @@
+public class KeepAlivePlus{
+    public static void main(String[] args) {
+        System.out.println("保活加QQ:2226864272");
+    }
+}
